af4252f6eed9999f741f9fa962eff98c3c38a604
DR鹿-MacBookAir <tw.julu@gmail.com>
2013-10-11 22:25:23 +0800

    加入 readableFilesize() 檔案大小易讀版本轉換

--- lib/utilities.php
+++ lib/utilities.php
@@ -100,6 +100,27 @@ function underline2ucwords( $str ){
     }
     return $res;
 }
+/**
+ * Returns a human readable filesize
+ *
+ * @author      wesman20 (php.net)
+ * @author      Jonas John
+ * @version     0.3
+ * @link        http://www.jonasjohn.de/snippets/php/readable-filesize.htm
+ */
+function readableFilesize($size) {
+ 
+    // Adapted from: http://www.php.net/manual/en/function.filesize.php
+ 
+    $mod = 1024;
+ 
+    $units = explode(' ','B KB MB GB TB PB');
+    for ($i = 0; $size > $mod; $i++) {
+        $size /= $mod;
+    }
+ 
+    return round($size, 2) . ' ' . $units[$i];
+}
 function errmsg( $errmsg='' ){
     if( APP::$systemConfigs['Production']==1 ) return ;
     
